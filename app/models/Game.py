e63fc11391e641d02f921b4fffc33a30af65d930
Rob <rob@robs-imac.home>
2020-10-30 19:47:54 +0000

    Setup finished - Made Enum for selection

---
+++ app/models/Game.py
@@ -0,0 +1,4 @@
+class Game:
+    def __init__(self, name):
+        self.name = name
+        self.win_count = 0
---
+++ app/models/selection.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Selection(Enum):
+    ROCK = 0,
+    PAPER = 1,
+    SCISSORS = 2
