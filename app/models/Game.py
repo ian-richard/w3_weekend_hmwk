2452ef738ed611b29c671a5d872c11604aadf404
Rob <rob@robs-imac.home>
2020-10-31 11:25:24 +0000

    Added get_winner() to Game - Maybe not the best way to do it

--- app/models/Game.py
+++ app/models/Game.py
@@ -1,6 +1,30 @@
+from app.models.selection import Selection
+
 class Game:
     def __init__(self, name, players):
         self.name = name
         self.players = players
         self.game_count = 0
 
+                                ##### Check first players selection and compare it to second players selection if they are not equal #####
+    def get_winner(self):
+        if self.players[0].selection == self.players[1].selection:
+            return None
+
+        if self.players[0].selection == Selection.ROCK:
+            if self.players[1].selection == Selection.SCISSORS:
+                return self.players[0]
+            if self.players[1].selection == Selection.PAPER:
+                return self.players[1]
+
+        if self.players[0].selection == Selection.PAPER:
+            if self.players[1].selection == Selection.SCISSORS:
+                return self.players[1]
+            if self.players[1].selection == Selection.ROCK:
+                return self.players[0]
+
+        if self.players[0].selection == Selection.SCISSORS:
+            if self.players[1].selection == Selection.ROCK:
+                return self.players[1]
+            if self.players[1].selection == Selection.PAPER:
+                return self.players[0]
--- tests/game_test.py
+++ tests/game_test.py
@@ -7,11 +7,58 @@ from app.models.selection import Selection
 
 class GameTest(unittest.TestCase):
     def setUp(self):
-        self.player1 = Player("Bob", Selection.ROCK)
-        self.player2 = Player("Barbera", Selection.PAPER)
-        self.player3 = Player("Margaret", Selection.SCISSORS)
+        self.rock = Player("Bob", Selection.ROCK)
+        self.paper = Player("Barbera", Selection.PAPER)
+        self.scissors = Player("Margaret", Selection.SCISSORS)
 
-        self.game = Game("Game 1", [self.player1, self.player2])
+        self.game = Game("Game 1", [self.rock, self.paper])
 
     def test_game_has_2_players(self):
         self.assertEqual(2, len(self.game.players))
+
+
+                    ##### ROCK TESTS #####
+    def test_game_has_winner_for_rock(self):
+        self.assertEqual(self.paper, self.game.get_winner())
+
+    def test_game_has_winner_for__rock(self):
+        self.game.players[1] = self.scissors
+        self.assertEqual(self.rock, self.game.get_winner())
+
+    def test_game_has_winner_for_draw(self):
+        self.game.players[1] = self.rock
+        self.assertEqual(None, self.game.get_winner())
+
+    
+                    ##### SCISSORS TESTS #####
+    def test_game_has_winner_for_scissors(self):
+        self.game.players[0] = self.scissors
+        self.game.players[1] = self.paper
+        self.assertEqual(self.scissors, self.game.get_winner())
+
+    def test_game_has_winner_for__scissors(self):
+        self.game.players[0] = self.scissors
+        self.game.players[1] = self.paper
+        self.assertEqual(self.scissors, self.game.get_winner())
+
+    def test_game_returns_none_for__draw(self):
+        self.game.players[0] = self.scissors
+        self.game.players[1] = self.scissors
+        self.assertEqual(None, self.game.get_winner())
+    
+
+                    ##### PAPER TESTS #####
+    def test_game_has_winner_for_paper(self):
+        self.game.players[0] = self.paper
+        self.game.players[1] = self.rock
+        self.assertEqual(self.paper, self.game.get_winner())
+
+    def test_game_has_winner_for__paper(self):
+        self.game.players[0] = self.paper
+        self.game.players[1] = self.scissors
+        self.assertEqual(self.scissors, self.game.get_winner())
+
+    def test_game_returns_none_for___draw(self):
+        self.game.players[0] = self.paper
+        self.game.players[1] = self.paper
+        self.assertEqual(None, self.game.get_winner())
